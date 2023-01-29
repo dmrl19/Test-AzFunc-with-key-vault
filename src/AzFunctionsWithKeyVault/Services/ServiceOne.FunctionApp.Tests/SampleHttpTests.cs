0f3772e2594ac074b78017a52dc53a8d5893d213
Daniel Moreano <daniel.moreano.learn@gmail.com>
2023-01-29 22:33:20 +0100

    Add pulumi commands & shell scripts to create/clean required resources. Include documentation.

--- src/AzFunctionsWithKeyVault/Services/ServiceOne.FunctionApp.Tests/SampleHttpTests.cs
+++ src/AzFunctionsWithKeyVault/Services/ServiceOne.FunctionApp.Tests/SampleHttpTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using FluentAssertions;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using ServiceOne.FunctionApp.Tests.Models;
 
@@ -12,8 +13,9 @@ public class SampleHttpTests
     public void GivenSampleHttpRequestData_SampleHttpRun_ShouldReturnSuccessfullyResponse()
     {
         var functionContextMock = new Mock<FunctionContext>();
-
-        var funcResponse = SampleHttp.Run(new SampleHttpRequestData(functionContextMock.Object), functionContextMock.Object);
+        var configurationMock = new Mock<IConfiguration>();
+        var function = new SampleHttp(configurationMock.Object);
+        var funcResponse = function.Run(new SampleHttpRequestData(functionContextMock.Object), functionContextMock.Object);
 
         funcResponse.Should().NotBeNull();
         funcResponse.StatusCode.Should().Be(HttpStatusCode.OK);
